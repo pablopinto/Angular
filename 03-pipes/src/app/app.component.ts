c790d4f5fe3e316111e971e9b43a4d35d795add8
Pablo Francisco Pinto Cebrián <pabloctender@gmail.com>
2023-02-10 10:53:43 +0100

    Subida del proyecto 03

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '03-pipes';
+
+  nombre: string = 'Capitan America'
+
+  nombre2: string = 'pAbLo FRanCIsCo PinTo cEbRiaN'
+
+  arreglo = ['Spiderman','Batman','Hulk','Aquaman','Linterna Verde','Thor','Loki','Groot','Iron Man','Scarlet Witch'];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.234;
+
+  salario: number = 1234.5;
+
+  fecha: Date = new Date();
+
+  videoUrl: string = 'https://www.youtube.com/embed/eBGIQ7ZuuiU';
+
+  activar: boolean = true;
+
+  valorPromesa = new Promise<string>( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('llegan los datos 4.5 segundos despues')
+    },4500)
+  } );
+
+  heroe ={
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion:{
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
--- 03-pipes/src/app/app.module.ts
+++ 03-pipes/src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es'
+
+//the second parameter 'es-ES' is optional
+registerLocaleData(localEs);
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue:'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
